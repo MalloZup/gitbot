12d622d1771584d91305c00467cc23ed351df5ff
Dario Maiocchi <dmaiocchi@suse.com>
2017-02-15 15:15:50 +0100

    Adding tests
    Modif the gitop library, refractor
    Adding more precise test for git_operation lib

--- lib/git_op.rb
+++ lib/git_op.rb
@@ -1,30 +1,30 @@
 #! /usr/bin/ruby
 
 require 'fileutils'
+require 'timeout'
 
 class GitOp
   attr_reader :git_dir
   def initialize(git_dir)
     @git_dir = git_dir
   end
-
   # this function merge the pr branch  into target branch,
   # where the author of pr wanted to submit
   def goto_prj_dir(repo)
+    git_repo_dir = @git_dir + "/" + repo.split('/')[1]
     # chech that dir exist, otherwise clone it
-    if File.directory?(@git_dir) == false ||
-       File.directory?(@git_dir + "/" + repo.split('/')[1])
+    if File.directory?(@git_dir) == false
       FileUtils.mkdir_p(@git_dir)
       Dir.chdir @git_dir
-      puts 'cloning the project in git_dir'
-      `git clone git@github.com:#{repo}.git`
+      puts `git clone git@github.com:#{repo}.git`
     end
-    # git_dir = /tmp/foo, we need to change to /tmp/foo/gitproject.git
-    if  @git_dir.include? repo.split('/')[1]
-      Dir.chdir @git_dir
+    begin
+      # /tmp/gitbot, this is in case the dir already exists
+      Dir.chdir git_repo_dir
+    rescue
+      # this is in case we clone the repo
+      Dir.chdir repo.split('/')[1]
     end
-    Dir.chdir @git_dir + "/" + repo.split('/')[1]
-
   end
 
   def check_git_dir
--- tests/gitop.rb
+++ tests/gitop.rb
@@ -1,22 +1,25 @@
 #! /usr/bin/ruby
 
 require_relative 'helper'
-
 require 'fileutils'
 
 class SimpleTest < Minitest::Test
-
   def test_basic
-     git = GitOp.new("gitty")
-     assert_equal('gitty', git.git_dir)
+    git = GitOp.new("gitty")
+    assert_equal('gitty', git.git_dir)
   end
   
   def test_goto_prj_dir_function
-     git = GitOp.new("gitty")
-     assert_equal('gitty', git.git_dir)
-     # test the case the dir with repo doesn't exists
-     git.goto_prj_dir("MalloZup/gitbot")
-     FileUtils.rm_rf('gitty/gitbot')     
+    git = GitOp.new("gitty")
+    # test the case the dir with repo doesn't exists
+    git.goto_prj_dir("MalloZup/gitbot")
+  end
+
+  def test_goto_prj_already_exist_dir
+    git = GitOp.new("gitty")
+    raise "dir should exist" if  File.directory?(git.git_dir) == false
+    git.goto_prj_dir("MalloZup/gitbot")
+    FileUtils.rm_rf('gitty')     
   end
 
 end
